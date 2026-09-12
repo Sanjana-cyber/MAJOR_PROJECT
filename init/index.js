6bc4ec2df7fc5619bc0e0a3124e5800b05a816e2
Mohan pandey <sanjanapandey29256@gamil.com>
2026-09-12 11:52:21 +0530

    third

---
+++ init/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const initdata=require("./data.js");
+const Lisiting=require("../MODELS/listing.js");
+
+
+//mongodb connection 
+let MONGO_URL="mongodb+srv://sanjanapandey29256_db_user:QoJAwb4BwzppLA1x@cluster0.bkxjhhl.mongodb.net/?appName=Cluster0"
+
+
+ async function main(){
+await mongoose.connect(MONGO_URL);
+}
+
+main()
+.then(()=>{
+    console.log("mongo db is connected");
+})
+
+.catch((err)=>{
+    console.log(err)
+})
+
+//inserion data in database
+const initDB=async ()=>{
+     await Listing.deleteMany({});
+      await Listing.insertMany(initdata.data);
+      console.log("data is initialized");
+}
+
+initDB();
